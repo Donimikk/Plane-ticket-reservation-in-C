d3add84cded3c5c2edda97464a457bf80ddfee29
Dominik Ivanov <ivanov.dominik@student.tuke.sk>
2025-01-30 13:28:12 +0100

    defined structs

---
+++ TicketReservation.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_PLANES 100
+
+struct Plane{
+    int pNumber;
+    int pCapacity;
+    int pAvailable;
+    int pPrice;
+    char pDestination[20];
+    char pOrigin[20];
+};
+
+int main(){
+
+}
